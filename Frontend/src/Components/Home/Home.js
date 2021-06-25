35e214673078cc2132bc4017ee5fa4c5e4cf0c56
krunalshastri00 <krunalshastri00@gmail.com>
2021-06-26 00:55:09 +0530

    Added menu for different types of mail

--- Frontend/src/Components/Home/Home.js
+++ Frontend/src/Components/Home/Home.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Button, notification, Menu } from 'antd';
+import 'antd/dist/antd.css';
+import styles from './Home.module.css';
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
 
 const Home = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div className={styles['header']}>
+        <h1>Scheduler</h1>
+        <div className={styles['header']}>
+          <h2>Avatar</h2>
+          <Button type='primary' danger shape='round'>
+            Logout
+          </Button>
+        </div>
+      </div>
+      <div style={{ width: 150, minHeight: '100vh' }}>
+        <Button
+          type='primary'
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          style={{ marginBottom: 16 }}
+        >
+          {React.createElement(
+            isCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined
+          )}
+        </Button>
+        <Menu
+          defaultSelectedKeys={['2']}
+          defaultOpenKeys={['sub1']}
+          mode='inline'
+          inlineCollapsed={isCollapsed}
+        >
+          <Menu.Item key='1' icon={<MailOutlined />}>
+            Compose
+          </Menu.Item>
+          <Menu.Item key='2' icon={<DesktopOutlined />}>
+            Scheduled
+          </Menu.Item>
+          <Menu.Item key='3' icon={<ContainerOutlined />}>
+            History
+          </Menu.Item>
+        </Menu>
+      </div>
+    </>
   );
 };
 
--- Frontend/src/Components/TitleBar/TitleBar.js
+++ Frontend/src/Components/TitleBar/TitleBar.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { AppBar, Avatar, Toolbar, Typography } from "@material-ui/core";
-import useStyles from "./styles";
-import logo from "../../images/logo.png";
+import React from 'react';
+import { AppBar, Avatar, Toolbar, Typography } from '@material-ui/core';
+import useStyles from './styles';
+import logo from '../../images/logo.png';
 
 const TitleBar = () => {
   const classes = useStyles();
   return (
-    <AppBar className={classes.appBar} position="static">
+    <AppBar className={classes.appBar} position='static'>
       <Toolbar>
-        <Avatar className={classes.avatar} variant="circle" src={logo} />
-        <Typography className={classes.typoGraphy} variant="h2">
-          Not Mailchimps
+        <Avatar className={classes.avatar} variant='circle' src={logo} />
+        <Typography className={classes.typoGraphy} variant='h2'>
+          Not Gmail
         </Typography>
       </Toolbar>
     </AppBar>

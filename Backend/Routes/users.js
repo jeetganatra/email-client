8bd34102d70946fe9be440d360a5a0920e79627a
krunalshastri00 <krunalshastri00@gmail.com>
2021-06-25 19:12:03 +0530

    Added auth route for signin

--- Backend/Routes/users.js
+++ Backend/Routes/users.js
@@ -42,6 +42,8 @@ router
 
         await user.save();
 
+        const profile = await User.findById(user.id).select('-password');
+
         const payload = {
           user: {
             id: user.id,
@@ -54,7 +56,61 @@ router
           { expiresIn: 360000 },
           (err, token) => {
             if (err) throw err;
-            res.json(token);
+            res.json({ profile, token });
+          }
+        );
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error!');
+      }
+    }
+  );
+
+router
+  .route('/signin')
+  .post(
+    body('email', 'Please include a valid email!').isEmail(),
+    body('password', 'Password required!').exists(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { email, password } = req.body;
+
+      try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid credentials!' }] });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid credentials!' }] });
+        }
+
+        const profile = await User.findById(user.id).select('-password');
+
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+
+        jwt.sign(
+          payload,
+          process.env.jwtSecret,
+          { expiresIn: 360000 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ profile, token });
           }
         );
       } catch (err) {

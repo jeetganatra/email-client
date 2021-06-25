6988205e2855dd10764329432f43d3e6e05947ab
krunalshastri00 <krunalshastri00@gmail.com>
2021-06-25 18:06:33 +0530

    Added user model

---
+++ Backend/Models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
